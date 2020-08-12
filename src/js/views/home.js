af2c7e3991fe75ceb0b6a89ed716a9606cf30d35
ALICARMONA <afacm94@gmail.com>
2020-08-12 06:36:32 +0000

    proyecto iniciado

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.scss";
+import ItemCard from "../component/ItemCard";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<div className="container">
+			<div className="card">
+				<img className="card-img-top" src="/images/pathToYourImage.png" alt="card-img-top" />
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">Some quick example text</p>
+					<a href="#!" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
